2fdf4546b2970be8038dd5399bc331a36a1ad05b
DoaaMouSSa <doaamoussa5@gmail.com>
2024-08-13 23:06:09 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,38 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { SideMenuComponent } from './adminPanel/side-menu/side-menu.component';
+import { ClientsComponent } from './home/clients/clients.component';
+import { FeaturesComponent } from './home/features/features.component';
+import { ContactComponent } from './home/contact/contact.component';
+import { AboutComponent } from './home/about/about.component';
+import { BrandsComponent } from './home/brands/brands.component';
+import { LoginComponent } from './authentication/login/login.component';
+import { RegisterComponent } from './authentication/register/register.component';
+import { NewestComponent } from './home/newest/newest.component';
+import { ServicesComponent } from './home/services/services.component';
+import { NavbarAdminComponent } from './adminPanel/navbar-admin/navbar-admin.component';
+import { StatisticsComponent } from './home/statistics/statistics.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    SideMenuComponent,
+    ClientsComponent,
+    FeaturesComponent,
+    ContactComponent,
+    AboutComponent,
+    BrandsComponent,
+    LoginComponent,
+    RegisterComponent,
+    NewestComponent,
+    ServicesComponent,
+    NavbarAdminComponent,
+    StatisticsComponent
   ],
   imports: [
     BrowserModule,
